4466ec051ac38ad3e944dd4158e03985440412e7
Josepc05 <153479884+Josepc05@users.noreply.github.com>
2023-12-11 07:52:30 -0600

    Update keys.ts

--- src/keys.ts
+++ src/keys.ts
@@ -1,8 +1,11 @@
-export default{
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
     database: {
-        host:'localhost',
-        user: 'root',
-        password:'',
-        database:'fishop'
+        host: 'database-fishop.cfydnegj4pay.use-east-2.rds.amazonaws.com',
+        user: 'admin',
+        password: 'root123*',
+        database: 'database-fishop',
+        port : '3306',
     }
-}
+};
