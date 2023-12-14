f76218a20f73e264a9a4c7000260b5d673c94f12
Josepc05 <153479884+Josepc05@users.noreply.github.com>
2023-12-13 18:36:13 -0600

    Update keys.js

--- src/build/keys.js
+++ src/build/keys.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'database-fishop.cfydnegj4pay.use-east-2.rds.amazonaws.com',
+        host: 'database-fishop.cfydnegj4pay.us-east-2.rds.amazonaws.com',
         user: 'admin',
         password: 'root123*',
         database: 'database-fishop',
