b932b69ca90ea3da74867561e8c245491580c9c9
Josepc05 <153479884+Josepc05@users.noreply.github.com>
2024-02-12 05:09:59 -0600

    Add files via upload

---
+++ src/build/keys.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        localhost: 'localhost:3000',
+        user: 'root',
+        password: '',
+        database: 'fishop',
+    }
+};
---
+++ src/routes/usuarioRoutes.ts
@@ -0,0 +1,20 @@
+﻿import { Router } from 'express';
+import { usuarioController } from '../controllers/usuarioController'
+import { validarToken } from '../middleware/auth';
+class usuarioRoutes {
+    public router: Router = Router();
+    constructor() {
+        this.config();
+    }
+    config(): void {
+        //this.router.get('/', (req, res) => res.send('probando ruta'));
+        this.router.post('/insertarUsuario/', usuarioController.insertar_usuario);
+        this.router.delete('/eliminarUsuario/:id', usuarioController.eliminar_usuario);
+        this.router.put('/modificarUsuario/:id', validarToken, usuarioController.modificar_usuario);
+        this.router.get('/mostrarTodosUsuario/', usuarioController.mostrar_Todos_usuario);
+        this.router.get('/mostrarUsuario/:id', validarToken, usuarioController.mostrar_usuario);
+        this.router.post('/ValidarUsuario/', usuarioController.ValidarUsuario);
+    }
+}
+const UsuarioRoutes = new usuarioRoutes();
+export default UsuarioRoutes.router;
