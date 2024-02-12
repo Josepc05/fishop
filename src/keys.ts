e3496b877d5b1342e1f20a87412e03aa2ac67cd1
Josepc05 <153479884+Josepc05@users.noreply.github.com>
2024-02-12 05:07:52 -0600

    Delete src directory

--- src/keys.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    database: {
-        host: 'database-fishop.cfydnegj4pay.us-east-2.rds.amazonaws.com',
-        user: 'admin',
-        password: 'root123*',
-        database: 'fishop',
-        port : 3306
-    }
-};
